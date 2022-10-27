19f55b0f937fd3df1d8fa6a23cbf848a30da6e0b
lmarcdev <lmarcdev@gmail.com>
2022-10-27 18:35:15 +0100

    added aboutcontent

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,33 +1,29 @@
 import "./WorkCardStyles.css";
+import WorkCard from "./WorkCard";
+import WorkCardData from "./WorkCradData";
+
 import React from 'react';
-import pro1 from "../assets/project1.png";
-import { NavLink } from "react-router-dom";
+
+
 const Work = () => {
   return (
     <div className="work-container">
         <h1 className="project-heading">Projects</h1>
         <div className="project-container">
-            <div className="project-card">
-                <img src={pro1} alt="imagee" />
-                <h2 className="project-title">
-                    Project Title</h2>
-                <div className="pro-details">
-                    <p>This is text</p>
-                    <div className="pro-btns">
-                        <NavLink to="url.com"
-                        className="btn">
-                            View
-                        </NavLink>
-                        <NavLink to="url.com"
-                        className="btn">
-                            Source
-                        </NavLink>
-                    </div>
-                </div>
-            </div>
+          {WorkCardData.map((val, ind) =>{
+            return(
+                <WorkCard
+                key={ind}
+                imgsrc={val.imgsrc}
+                title={val.title}
+                text={val.text}
+                view={val.view}
+                />
+            );
+          })}  
         </div>
     </div>
-  )
-}
+  );
+};
 
 export default Work
---
+++ src/components/WorkCradData.js
@@ -0,0 +1,24 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+const ProjectCardData = [
+    {
+       imgsrc: pro1,
+       title: "Ecommerce Website Design",
+       Text: "Thriving FB community and top-renked podcast this is a very long text asdsasddasdasdsa",
+       view: "https://www.youtube.com/watch?v=0h2b4ftbZcU&ab_channel=Tech2etc",
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS/Course Website Design",
+        Text: "Podcast FB community and top-renked podcast this is a very long text asdsasddasdasdsa",
+        view: "https://www.youtube.com/watch?v=0h2b4ftbZcU&ab_channel=Tech2etc",
+     },
+     {
+        imgsrc: pro3,
+        title: "Rymo Ecommerce Design",
+        Text: "Top Ranked  FB community and top-renked podcast this is a very long text asdsasddasdasdsa",
+        view: "https://www.youtube.com/watch?v=0h2b4ftbZcU&ab_channel=Tech2etc",
+     },
+];
+export default ProjectCardData;
